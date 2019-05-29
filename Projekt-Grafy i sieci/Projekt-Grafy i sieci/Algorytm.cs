9952adbfef2054fa5a42325c646c214f7cc9a513
JulaHula <julia.stencel@gmail.com>
2019-05-29 12:52:06 +0200

    Update Algorytm.cs

--- Projekt-Grafy i sieci/Projekt-Grafy i sieci/Algorytm.cs
+++ Projekt-Grafy i sieci/Projekt-Grafy i sieci/Algorytm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace algorytm2
 {
@@ -9,11 +9,6 @@ namespace algorytm2
             public int dist { get; set; } //dystans
             public int prev { get; set; } //poprzedniks
             public bool odwiedzony { get; set; }
-
-            public Wierzcholek()
-            {
-
-            }
         };
 
         private static int ZnajdzMinDist(Wierzcholek[] tab)
@@ -22,7 +17,7 @@ namespace algorytm2
             int mindist = 2147483647;
             for (int i = 0; i < tab.Length; i++)
             {
-                if (tab[i].odwiedzony == false && tab[i].dist < mindist)
+                if (tab[i].odwiedzony == false && tab[i].dist < mindist) //zwracamy wierzcholek, jesli go jeszcze nie badalismy i ma najmniejszy dist
                 {
                     min = i;
                     mindist = tab[i].dist;
@@ -31,35 +26,42 @@ namespace algorytm2
             return min;
         }
 
-        public static int Dijkstry(int[,] macierz, int startowy)
+        public static int Dijkstry(int[,] macierz, int startowy) //zwraca sumę najmniejszych odleglosci od danego wierzcholka do wszystkich pozostałych
         {
             //przygotowanie macierzy, dla wierzch startowego dist = 0
             int iloscW = macierz.GetLength(0);
             Wierzcholek[] tab = new Wierzcholek[iloscW];
+
+
             for (int i = 0; i < iloscW; i++)
             {
                 if (i == startowy)
                 {
-                    tab[i].dist = 0;
-                    tab[i].odwiedzony = false;
-                    tab[i].prev = -1;
+                    Wierzcholek w = new Wierzcholek();
+                    w.dist = 0;
+                    w.odwiedzony = false;
+                    w.prev = -1;
+                    tab[i] = w;
                 }
                 else
                 {
-                    tab[i].dist = 2147483647; //zamiast nieskonczonosci
-                    tab[i].odwiedzony = false;
-                    tab[i].prev = -1;
+                    Wierzcholek w = new Wierzcholek();
+                    w.dist = 21474836;
+                    w.odwiedzony = false;
+                    w.prev = -1;
+                    tab[i] = w;
                 }
             }
 
-            int u = startowy;
-            while (u != -1)
+            //ZACZYNAMY NASZ ALGORYTM :D
+            int u = startowy; //nasze poszukiwania zaczniemy od badań względem wierzch. wskazanego przez uzytkownika.
+            while (u != -1) //pozniej u będzie się zmieniać, gdy wszystkie będą odiwedzone met. pom. "Znajdz Min Dist wypluje -1";
             {
-                tab[u].odwiedzony = true;
+                tab[u].odwiedzony = true; //zapamiętajmy, że już wzgledem niego badalismy
                 for (int i = 0; i < iloscW; i++)
                 {
-                    if (macierz[u, i] > 0)
-                        if (tab[u].dist + macierz[u, i] < tab[i].dist)
+                    if (macierz[u, i] > 0) //jesli nie ma krawedzi miedzy wierzcholkami to nie ma co szukac, tam bedzie "-1"
+                        if (tab[u].dist + macierz[u, i] < tab[i].dist) //jesli znaleziony dist jest mniejszy od zapamietanego to zamieniamy
                         {
                             {
                                 tab[i].dist = tab[u].dist + macierz[u, i];
@@ -67,20 +69,33 @@ namespace algorytm2
                             }
                         }
                 }
-                u = ZnajdzMinDist(tab);
-                
+                u = ZnajdzMinDist(tab); //teraz bedziemy szukać względem kolejnego wierzchołka - o najmniejszym na razie dystansie
             }
-            Wierzcholek wynik = tab[0];
-            int nazwWierzcholka = 0;
-            for (int i = 1; i < tab.Length; i++)
+
+            //sumujemy odleglosci
+            int suma = 0;
+            for (int i = 0; i < tab.Length; i++)
             {
-                if(tab[i].dist>wynik.dist)
+                suma = suma + tab[i].dist;
+            }
+            return suma;
+        }
+
+        public static int BestPlace(int[,] macierz) //szukamy takiego wierzchołka, ktorego suma odleglosci do pozostałych jest najniejsza
+        {
+            int najlepszeMiejsce = -1;
+            int najlepszaSuma = 21474836;
+            for (int i = 0; i < macierz.GetLength(0); i++)
+            {
+                int suma = Dijkstry(macierz, i);
+                if (suma < najlepszaSuma)
                 {
-                    wynik = tab[i];
-                    nazwWierzcholka = i;
+                    najlepszeMiejsce = i;
+                    najlepszaSuma = suma;
                 }
             }
-            return nazwWierzcholka;
-        }
+            return najlepszeMiejsce;
+        }       
     }
 }
+
